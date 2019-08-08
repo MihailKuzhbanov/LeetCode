949d8da69f22e903b0f8320ba516786d30611498
Mihail <scrayerful@gmail.com>
2019-08-09 00:33:52 +0500

    Initial commit

---
+++ RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,20 @@
+//16ms 9.8mb
+//above 99% results
+
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		const int c(nums.size());				//not works with dynamic vectors in real app
+		int i = 0;
+		for (int j = 0; j < c; j++)
+		{
+			if (nums[i] != nums[j])
+			{
+				i++;
+				nums[i] = nums[j];
+			}
+		}
+
+		return (c > 1) ? i + 1 : c;
+	}
+};
