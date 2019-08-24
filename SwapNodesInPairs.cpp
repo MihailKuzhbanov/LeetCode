f89da19dd3a95e081c2f7176acacceef9ba5b183
Mihail <scrayerful@gmail.com>
2019-08-24 23:53:29 +0500

    Update SwapNodesInPairs.cpp

--- SwapNodesInPairs.cpp
+++ SwapNodesInPairs.cpp
@@ -3,11 +3,11 @@
 
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) {
+    ListNode* swapPairs(ListNode* head)
+    {
         if(!head || !head->next) return head;
-        ListNode* temp;
-        temp = head->next;
-        head->next = swapPairs(head->next->next);
+        ListNode* temp = head->next;
+        head->next = swapPairs(temp->next);
         temp->next = head;
 
         return temp;
