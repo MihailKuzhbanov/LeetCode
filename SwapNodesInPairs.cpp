ee305262b81633e16b5e38accf9ba25baa7aeb69
Mihail <scrayerful@gmail.com>
2019-08-24 23:53:50 +0500

    Revert "Update SwapNodesInPairs.cpp"
    This reverts commit f89da19dd3a95e081c2f7176acacceef9ba5b183.

--- SwapNodesInPairs.cpp
+++ SwapNodesInPairs.cpp
@@ -3,11 +3,11 @@
 
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head)
-    {
+    ListNode* swapPairs(ListNode* head) {
         if(!head || !head->next) return head;
-        ListNode* temp = head->next;
-        head->next = swapPairs(temp->next);
+        ListNode* temp;
+        temp = head->next;
+        head->next = swapPairs(head->next->next);
         temp->next = head;
 
         return temp;
