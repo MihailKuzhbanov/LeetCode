e60e78b590eb54f9624ff222c7c8e1eb38a73256
Mihail <scrayerful@gmail.com>
2019-08-24 23:54:53 +0500

    task 24

---
+++ SwapNodesInPairs.cpp
@@ -0,0 +1,14 @@
+//task 24
+//0ms 8.5mb
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head)
+    {
+        if(!head || !head->next) return head;
+        ListNode* temp = head->next;
+        head->next = swapPairs(temp->next);
+        temp->next = head;
+        return temp;
+    }
+};
