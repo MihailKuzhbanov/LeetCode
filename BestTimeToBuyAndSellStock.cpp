8c164495de44c8447c4b44df53b319a98860f70c
Mihail <scrayerful@gmail.com>
2019-10-05 20:12:59 +0500

    task 121

---
+++ BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,17 @@
+//task 121
+//8ms 9.5mb
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        if (prices.empty()) return 0;
+        int buy(prices[0]), profit(0);
+        for (int i = 1; i < prices.size(); i++)
+        {
+            buy = min(buy, prices[i]);
+            profit = max(profit, prices[i] - buy);
+        }
+        return profit;
+    }
+};
